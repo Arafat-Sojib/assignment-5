f12e447faed47ea1b6a80331a1c118f815092bc0
ArafatIslamSojib <arafatislam.sojib2006@gmail.com>
2025-08-27 21:59:55 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,48 @@
+let heartCount = 0;
+let copyCount = 0;
+let coinCount = 100;
+
+function incrementHeart() {
+  heartCount++;
+  document.getElementById("heartCount").textContent = heartCount;
+}
+
+function copyNumber(number) {
+  navigator.clipboard.writeText(number);
+  alert("Hotline Number Copied: " + number);
+  copyCount++;
+  document.getElementById("copyCount").textContent = copyCount;
+}
+
+function callNumber(title, number) {
+  if (coinCount >= 20) {
+    alert("Calling " + title + ": " + number);
+    coinCount -= 20;
+    document.getElementById("coinCount").textContent = coinCount;
+
+    const now = new Date();
+    const timeStr = now.toLocaleTimeString();
+    const container = document.createElement("div");
+    container.className = "flex justify-between items-center mt-2 border-1 border-gray-200 bg-gray-50 rounded-lg shadow-lg p-2";
+
+    const leftPart = document.createElement("div");
+    leftPart.innerHTML = `
+      <p class="font-semibold">${title}</p>
+      <p class="text-sm text-gray-600">${number}</p>
+    `;
+
+    const rightPart = document.createElement("div");
+    rightPart.innerHTML = `<span class="text-xs">${timeStr}</span>`;
+
+    container.appendChild(leftPart);
+    container.appendChild(rightPart);
+
+    document.getElementById("callHistory").appendChild(container);
+  } else {
+    alert("Not enough coins to make a call!");
+  }
+}
+
+function clearHistory() {
+  document.getElementById("callHistory").innerHTML = "";
+}
